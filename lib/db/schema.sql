91d2a5e370191a6f5d69ea5c35971c69c1aa48d8
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-10 00:14:18 -0500

    updates to package.json

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,24 +1,31 @@
 -- drops database if it exists --
 DROP DATABASE IF EXISTS werewolf_DB;
 -- creates database --
-CREATE database werewolf_DB;
+CREATE DATABASE werewolf_DB;
 
 -- use database --
 USE werewolf_DB;
 
--- create tables --
+-- create tables within database --
+CREATE TABLE department (
+     -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- Makes a string column called "name" which cannot contain null --
+    name VARCHAR(30) NOT NULL
+);
 
---departments --
-    -- id --
-    -- name --
-
--- roles --
+CREATE TABLE role ()
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL() NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY ()
     -- id --
     -- title --
     -- salary --
     -- department id --
     -- key? --
-
+CREATE TABLE employee ()
 -- employees --
     -- id --
     -- first name --
