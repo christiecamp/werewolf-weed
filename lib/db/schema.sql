b684d292ee359c1398c1ae20b04fdb1ebdac3715
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-11-09 23:37:26 -0500

    pseudocoding schema structure

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,1 +1,28 @@
--- create database & tables
+-- drops database if it exists --
+DROP DATABASE IF EXISTS werewolf_DB;
+-- creates database --
+CREATE database werewolf_DB;
+
+-- use database --
+USE werewolf_DB;
+
+-- create tables --
+
+--departments --
+    -- id --
+    -- name --
+
+-- roles --
+    -- id --
+    -- title --
+    -- salary --
+    -- department id --
+    -- key? --
+
+-- employees --
+    -- id --
+    -- first name --
+    -- last name --
+    -- role id --
+    -- manager id --
+    -- key? --
